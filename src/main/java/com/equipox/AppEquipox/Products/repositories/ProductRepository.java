cce3a26cdc9f34cb7b90e9ab6ca75516e359ab42
KevinPm-Uniajc <stevenkspm92@gmail.com>
2025-04-19 01:09:59 -0500

    1. Se modica la conexion a la base de datos 2. Se ajusta metodo del repositorio de productos

--- src/main/java/com/equipox/AppEquipox/Products/repositories/ProductRepository.java
+++ src/main/java/com/equipox/AppEquipox/Products/repositories/ProductRepository.java
@@ -11,23 +11,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
 
-
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
 
     List<ProductModel> findByDescription(String description);
+
     List<ProductModel> findByCategory(String category);
 
-    @Query("SELECT p FROM ProductModel p WHERE " +
-       "(:category IS NULL OR p.category = :category) AND " +
-       "(:minPrice IS NULL OR p.vlrVenta >= :minPrice) AND " +
-       "(:maxPrice IS NULL OR p.vlrVenta <= :maxPrice) AND " +
-       "(:stockAvailable IS NULL OR p.stock > 0)")
+    /*
+     * @Query("SELECT p FROM ProductModel p WHERE " +
+     * "(:category IS NULL OR p.category = :category) AND " +
+     * "(:minPrice IS NULL OR p.vlrVenta >= :minPrice) AND " +
+     * "(:maxPrice IS NULL OR p.vlrVenta <= :maxPrice) AND " +
+     * "(:stockAvailable IS NULL OR p.stock > 0)")
+     */
 
-       List<ProductModel> filterProducts(
-        @Param("category") String category,
-        @Param("minPrice") BigDecimal minPrice,
-        @Param("maxPrice") BigDecimal maxPrice,
-        @Param("stockAvailable") Boolean stockAvailable);
+    @Query("SELECT p FROM ProductModel p WHERE " +
+            "(:category IS NULL OR p.category = :category) AND " +
+            "(:minPrice IS NULL OR p.vlrVenta >= :minPrice) AND " +
+            "(:maxPrice IS NULL OR p.vlrVenta <= :maxPrice) ")
+
+    List<ProductModel> filterProducts(
+            @Param("category") String category,
+            @Param("minPrice") BigDecimal minPrice,
+            @Param("maxPrice") BigDecimal maxPrice,
+            @Param("stockAvailable") Boolean stockAvailable);
 
 }
